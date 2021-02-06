2295ba5c2db7cd205ef5e088cd08c9c1dcc9cf3c
Hogar <nikolas.bernal@mail.escuelaing.edu.co>
2021-02-05 20:52:25 -0500

    AppTest modificado

--- src/test/java/edu/escuelaing/arep/heroku/AppTest.java
+++ src/test/java/edu/escuelaing/arep/heroku/AppTest.java
@@ -11,15 +11,71 @@ public class AppTest
         extends TestCase
 {
     @Test
-    public void testMeanCorrect(){
+    public void testColumn1MeanCorrect(){
         LinkedList list = new LinkedList();
-        list.addNode(5.0);
-        list.addNode(1.0);
-        list.addNode(10.0);
-        list.addNode(2.0);
-        list.addNode(7.0);
+        list.addNode(160);
+        list.addNode(591);
+        list.addNode(114);
+        list.addNode(229);
+        list.addNode(230);
+        list.addNode(270);
+        list.addNode(128);
+        list.addNode(1657);
+        list.addNode(624);
+        list.addNode(1503);
         Operations operations = new Operations(list);
-        assertEquals(5.0,operations.mean());
+        assertEquals(550.6,operations.mean());
     }
+
+    /*@Test
+    public void testColumn1StdDevCorrect(){
+        LinkedList list = new LinkedList();
+        list.addNode(160);
+        list.addNode(591);
+        list.addNode(114);
+        list.addNode(229);
+        list.addNode(230);
+        list.addNode(270);
+        list.addNode(128);
+        list.addNode(1657);
+        list.addNode(624);
+        list.addNode(1503);
+        Operations operations = new Operations(list);
+        assertEquals(572.03,operations.stdDev());
+    }
+
+    @Test
+    public void testColumn2MeanCorrect(){
+        LinkedList list = new LinkedList();
+        list.addNode(15.0);
+        list.addNode(69.9);
+        list.addNode(6.5);
+        list.addNode(22.4);
+        list.addNode(28.4);
+        list.addNode(65.9);
+        list.addNode(19.4);
+        list.addNode(198.7);
+        list.addNode(38.8);
+        list.addNode(138.2);
+        Operations operations = new Operations(list);
+        assertEquals(60.32 ,operations.mean());
+    }
+
+    @Test
+    public void testColumn2StdDevCorrect(){
+        LinkedList list = new LinkedList();
+        list.addNode(15.0);
+        list.addNode(69.9);
+        list.addNode(6.5);
+        list.addNode(22.4);
+        list.addNode(28.4);
+        list.addNode(65.9);
+        list.addNode(19.4);
+        list.addNode(198.7);
+        list.addNode(38.8);
+        list.addNode(138.2);
+        Operations operations = new Operations(list);
+        assertEquals(62.26,operations.stdDev());
+    }*/
 
 }
